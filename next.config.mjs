aac5c276416ea36b642a1f3a0e753a832f26c462
catcuts <354151733@qq.com>
2024-07-03 15:39:04 +0800

    feat: 第10章-配置 dashboard 部分预渲染。

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        ppr: 'incremental',  // 'incremental' 值允许您对特定路由采用 PPR。
+    }
+};
 
 export default nextConfig;
