f3da421ea7bc3082ec6d14482bffb99002d368b7
catcuts <354151733@qq.com>
2024-07-04 14:16:51 +0800

    feat: 第14章-支持创建新发票时对字段内容相应进行错误提示。

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,9 +7,17 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        required_error: 'Please select a customer.',
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        required_error: 'Please select an invoice status.',
+        invalid_type_error: 'Please select an invoice status.'
+    }),
     date: z.string(),
 });
 
@@ -17,16 +25,31 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    // const rawFormData = CreateInvoice.parse({
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    // const rawFormData = CreateInvoice.safeParse({
     //     customerId: formData.get('customerId'),
     //     amount: formData.get('amount'),
     //     status: formData.get('status'),
     //   });
     // 从表单数据中提取、解析、校验创建新发票所需数据
-    const rawFormData = CreateInvoice.parse(Object.fromEntries(formData.entries()));
-
-    const { customerId, amount, status } = rawFormData;
+    const validatedFields = CreateInvoice.safeParse(Object.fromEntries(formData.entries()));
+    // 如果校验失败，返回错误消息，否则继续
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
